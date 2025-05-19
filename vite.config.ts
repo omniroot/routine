ed9512b92c8ecd6464e8d42712a9679127a9c8f4
omniroot <omnirootofc@gmail.com>
2025-05-19 15:41:49 +0300

    Add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import stylex from "vite-plugin-stylex";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // @ts-ignore
+  plugins: [react(), stylex({})],
+  resolve: {
+    alias: {
+      "@pages": path.resolve(__dirname, "src", "pages"),
+      "@features": path.resolve(__dirname, "src", "components", "features"),
+      "@ui": path.resolve(__dirname, "src", "components", "ui"),
+      "@components": path.resolve(__dirname, "src", "components"),
+      "@": path.resolve(__dirname, "src"),
+      "~": path.resolve(__dirname),
+    },
+  },
+});
