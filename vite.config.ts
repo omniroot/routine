5aa004d6eb6936414a791dc1f8a7d5fcadb73b1b
omniroot <omnirootofc@gmail.com>
2025-05-20 16:12:58 +0300

    Update aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import stylex from "vite-plugin-stylex";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // @ts-ignore
-  plugins: [react(), stylex({})],
+  plugins: [react(), svgr({})],
   resolve: {
     alias: {
       "@pages": path.resolve(__dirname, "src", "pages"),
-      "@features": path.resolve(__dirname, "src", "components", "features"),
       "@ui": path.resolve(__dirname, "src", "components", "ui"),
-      "@components": path.resolve(__dirname, "src", "components"),
+      "@widgets": path.resolve(__dirname, "src", "components", "widgets"),
       "@": path.resolve(__dirname, "src"),
       "~": path.resolve(__dirname),
     },
